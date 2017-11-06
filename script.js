dbd5298fd475555a909996f17d9e013f27453d2e
Marina Mezvinsky <marmez08@gmail.com>
2017-11-06 17:25:43 +0200

    4-3

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 function addImg(x) {
     if (x===italy){
-        document.getElementById("p1").innerHTML = "italy";
+        document.getElementById("p1").innerHTML = "Italy";
     }
    else if (x===israel){
-        document.getElementById("p1").innerHTML = "israel";
+        document.getElementById("p2").innerHTML = "Israel";
     }
    else if (x===holland){
-        document.getElementById("p1").innerHTML = "holland";
+        document.getElementById("p3").innerHTML = "Holland";
+    }
+    else if (x===usa){
+        document.getElementById("p4").innerHTML = "U.S.A";
+    } 
+    else if (x===france){
+        document.getElementById("p5").innerHTML = "France";
     }
 }
 function removeImg(y){
     
-        document.getElementById("p1").innerHTML ="" ; 
+    document.getElementById("p1").innerHTML ="" ; 
+    document.getElementById("p2").innerHTML ="" ; 
+    document.getElementById("p3").innerHTML ="" ; 
+    document.getElementById("p4").innerHTML ="" ; 
+    document.getElementById("p5").innerHTML ="" ;    
+    
     }
   
