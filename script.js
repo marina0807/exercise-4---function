4b90f4a9006049154c5bb4f7b6edaf0faaaca847
Marina Mezvinsky <marmez08@gmail.com>
2017-11-07 09:11:29 +0200

    exercise 4-4

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ function addImg(x) {
         document.getElementById("p1").innerHTML = "Italy";
     }
    else if (x===israel){
-        document.getElementById("p2").innerHTML = "Israel";
+        document.getElementById("p1").innerHTML = "Israel";
     }
    else if (x===holland){
-        document.getElementById("p3").innerHTML = "Holland";
+        document.getElementById("p1").innerHTML = "Holland";
     }
     else if (x===usa){
-        document.getElementById("p4").innerHTML = "U.S.A";
+        document.getElementById("p1").innerHTML = "U.S.A";
     } 
     else if (x===france){
-        document.getElementById("p5").innerHTML = "France";
+        document.getElementById("p1").innerHTML = "France";
     }
 }
 function removeImg(y){
     
     document.getElementById("p1").innerHTML ="" ; 
-    document.getElementById("p2").innerHTML ="" ; 
+  /*  document.getElementById("p2").innerHTML ="" ; 
     document.getElementById("p3").innerHTML ="" ; 
     document.getElementById("p4").innerHTML ="" ; 
     document.getElementById("p5").innerHTML ="" ;    
-    
+    */
     }
   
